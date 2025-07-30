b855d654d6c7e080aef34fd4d46027f46beb0a6e
Dibesh Ranjan <dibeshranjan6@gmail.com>
2025-07-30 13:10:49 +0530

    comment complete on String folder

--- strings/FindLength.java
+++ strings/FindLength.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class FindLength {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); // here we create Scanner class object
 
-        System.out.print("Enter a String : ");
-        String s = sc.nextLine();
+        System.out.print("Enter a String : "); // here we want user input
+        String s = sc.nextLine();  // here we got the user input value
 
-        System.out.println("Length of " + s + " is : " + s.length());
+        System.out.println("Length of " + s + " is : " + s.length());  // here we print the length of the value
 
-        sc.close();
+        sc.close();  //  here we close our Scanner class object
     }
 }
--- strings/ReverseString.java
+++ strings/ReverseString.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class ReverseString {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); // here we create a Scanner class object
 
-        System.out.print("Enter a String : ");
-        String text = sc.nextLine();
+        System.out.print("Enter a String : "); // here we want an user input
+        String text = sc.nextLine();  // here we got the user value
 
-        String newText = "";
+        String newText = "";  // here we set a demo variable for concatenation
 
-        for (int i = text.length() - 1; i >= 0; i--) {
-            newText += text.charAt(i);
+        for (int i = text.length() - 1; i >= 0; i--) { // here the loop is running until the text length is 0
+            newText += text.charAt(i); // here we concatenate our String [newText = descending order]
         }
 
-        System.out.println("Original String: " + text);
-        System.out.println("After Reversing the String: " + newText);
+        System.out.println("Original String: " + text); // here we print original String
+        System.out.println("After Reversing the String: " + newText); // here we print reverse String
 
-        sc.close();
+        sc.close(); // here we close our Scanner object class
     }
 }
 
